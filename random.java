c402d1f7778847eab984d4f04774d57a24c3b4ff
Victor Antony <32144387+vantony1@users.noreply.github.com>
2019-09-24 17:02:52 +0200

    Update random.java

--- random.java
+++ random.java
@@ -1,9 +1,14 @@
+/*
+
+Agent that plays Connect4 using random moves
+
+*/
+
 
 public class random extends ai {
 
 	public random(String name, int number, int opponent) {
 		super(name, number, opponent);
-		// TODO Auto-generated constructor stub
 	}
 	
 	public int get_next_move() {
