20a6896e07f243fb6aa04908513082864106ba46
Victor Antony <32144387+vantony1@users.noreply.github.com>
2019-09-24 17:01:44 +0200

    Update minimax_ultimate.java

--- minimax_ultimate.java
+++ minimax_ultimate.java
@@ -7,46 +7,30 @@ public class minimax_ultimate extends ai {
 		public minimax_ultimate(String name, int number, int opponent, int depth) {
 			super(name, number, opponent);
 			this.depth = depth;
-			// TODO Auto-generated constructor stub
 		}
 		
 		public int get_next_move(board current) {
-			//current.get_board();
-			//System.out.println("Returning board");
 			board test = current.copy();
 			System.out.println("thinking about the next optimal move - I sense fear in you mortal");
-			//test.get_board();
 			int alpha = -10000;
 			int beta = 10000;
 			int choice = minimax_a_b_algorithm(test, this.number, this.opponent, this.number, true, 1, alpha, beta, this.depth);
-			//System.out.println("END MINMAX BOARD");
 			System.out.println("CHOICE: " + choice);
-			//test.get_board();
-			//System.out.println("Returning board");
 			return choice;
 		}
 		
 	public int minimax_a_b_algorithm(board board, int number, int opponent, int m, boolean first, int count, int alpha, int beta, int depth) {
-			
-			//System.out.println("printing board inputed into minimax");
-			//board.get_board();
-			//System.out.println("WHOSE MOVE = " + m);
-			
+
 			if(board.eval_board() != 0 || board.end_board()) {
-				
 				int u = board.utility(board, number, opponent);
-				//System.out.println("UTILITY = " + u);
 				return u*1000;
 				
 			} else if (count == depth) {
 				
 				int h = board.heuristic(number, opponent);
-				//System.out.println("UTILITY = " + h);
 				return h;
 				
 			} else if(m == number) {
-				
-				//System.out.println("MAX -- alpha");
 				
 				ArrayList<move> moves = new ArrayList<move>();
 				ArrayList<board> next_boards = new ArrayList<board>();
@@ -58,10 +42,8 @@ public class minimax_ultimate extends ai {
 				}
 				
 				for (move move : moves) {
-				//	System.out.println("result for move " + move.col);
 					board test = board.copy();
 					board next = test.result(number, move.col, test);
-				//	next.get_board();
 					next_boards.add(next);
 				}
 				
@@ -72,32 +54,18 @@ public class minimax_ultimate extends ai {
 					alpha = Math.max(alpha, moves.get(i).value);
 					
 					if(first) {
-						//System.out.println("FIRST ANALYSIS at i = " + i);
-						//System.out.println("Value for move " + moves.get(i).col + " & value = " + moves.get(i).value);
 					} else if (beta <= alpha){
 						break;
 					}
 				}
 				
-				/*System.out.println("MAX MOVES: at move " + count);
-				for(move move : moves) {
-					System.out.println("MOVE " + move.col + " has value " +  move.value);
-				}*/
-		
 				move max = moves.get(0);
-				
-				//System.out.println("EVAL FOR MAX MOVES at move " + count);
-				
+								
 				for (move move : moves) {
-					//System.out.println("Max move : " + max.col + " Value = " + max.value);
-					//System.out.println("Move move : " + move.col + " Value = " + move.value);
 					if(move.value > max.value) {
 						max = move;
 					}
 				}
-				
-				//System.out.println("Max Returning Move" + max.col +  "with value = " + max.value);
-				
 				
 				if(first) {
 					return max.col;
@@ -108,9 +76,7 @@ public class minimax_ultimate extends ai {
 				
 				
 			} else {
-				
-				//System.out.println("MIN");
-				
+
 				ArrayList<move> moves = new ArrayList<move>();
 				ArrayList<board> next_boards = new ArrayList<board>();
 				ArrayList<Integer> values = new ArrayList<Integer>(); 
@@ -121,10 +87,8 @@ public class minimax_ultimate extends ai {
 				}
 				
 				for (move move : moves) {
-					//System.out.println("result for move " + move.col);
 					board test = board.copy();
 					board next = test.result(opponent, move.col, test);
-					//next.get_board();
 					next_boards.add(next);
 				}
 				
@@ -138,27 +102,15 @@ public class minimax_ultimate extends ai {
 						break;
 					}
 				}
-				
-				/*System.out.println("MIN MOVES: at move " + count);
-				for(move move : moves) {
-					System.out.println("MOVE " + move.col + " has value " +  move.value);
-				}
-		*/
+		
 				move min = moves.get(0);
-				
-				//System.out.println("EVAL FOR MIN MOVES at move " + count);
-				
+			
 				for (move move : moves) {
-					//System.out.println("Min move : " + min.col + " Value = " + min.value);
-					//System.out.println("Move move : " + move.col + " Value = " + move.value);
 					if(move.value < min.value) {
 						min = move;
 					}
 				}
-				
-				//System.out.println("MIN -- Returning Move" + min.col +  "with value =  " + min.value);
-				
-				
+			
 				if(first) {
 					return min.col;
 				} else {
@@ -169,6 +121,8 @@ public class minimax_ultimate extends ai {
 			
 		}
 
+	
+	///THE CODE BELOW IS AN OLDER IMPLEMENTATION OF THE FUNCTION ABOVE
 		
 		/*public int minimax_a_b_algorithm(board board, int number, int opponent, int m, boolean first, int count, int holder) {
 			
